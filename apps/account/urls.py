1d8434cf4cd07e79be894847920ffc010ab139c5
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2025-04-30 20:10:25 +0500

    fix: change url name

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from apps.account.views import ( TelegramLoginView )
+from apps.account.views import ( TelegramAuthAPIView )
 
 urlpatterns = [
-	path('auth/telegram/', TelegramLoginView.as_view(), name='telegram-login'),
+	path('auth/telegram/', TelegramAuthAPIView.as_view(), name='telegram-login'),
 	path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
 
