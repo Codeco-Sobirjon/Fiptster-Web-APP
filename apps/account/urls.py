71d90314a09b7d9909c2996b24d0b0cbadb5ab0e
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2025-05-01 21:56:40 +0500

    modified: add user profile api

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from apps.account.views import ( TelegramAuthAPIView )
+from apps.account.views import ( TelegramAuthAPIView, UserProfileAPIView )
 
 urlpatterns = [
 	path('auth/telegram/', TelegramAuthAPIView.as_view(), name='telegram-login'),
 	path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+
+	path('profile/', UserProfileAPIView.as_view(), name='user-profile'),
 ]
 
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,13 +1,16 @@
 import json
 import urllib
 
-from rest_framework.permissions import AllowAny
+from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from apps.account.models import CustomUser, UserProfile
+from apps.account.serializers import CustomUserSerializer
 from apps.account.utils.telegram_auth import check_auth, TOKEN
 
 
@@ -40,6 +43,7 @@ class TelegramAuthAPIView(APIView):
             username = user_data.get("username", f"tg_{telegram_id}")
             first_name = user_data.get("first_name", "")
             last_name = user_data.get("last_name", "")
+            image_url = user_data.get("photo_url", "")
 
             user = CustomUser.objects.filter(tg_id=telegram_id).first()
 
@@ -48,7 +52,8 @@ class TelegramAuthAPIView(APIView):
                 defaults={
                     'username': username,
                     'first_name': first_name,
-                    'last_name': last_name
+                    'last_name': last_name,
+                    'avatar': image_url,
                 }
             )
             if created:
@@ -64,3 +69,17 @@ class TelegramAuthAPIView(APIView):
             return Response({'error': 'Неверный формат JSON'}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({'error': f'Ошибка сервера: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class UserProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        operation_description="Get current user's profile",
+        responses={200: CustomUserSerializer()}
+    )
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        user_profile = get_object_or_404(UserProfile, user=user)
+        serializer = CustomUserSerializer(user_profile, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
