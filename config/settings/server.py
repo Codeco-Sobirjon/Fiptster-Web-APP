b1c1c06f9d33174c1d93605499e9c0f656607a84
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2025-05-12 11:40:22 +0500

    changed

--- config/settings/server.py
+++ config/settings/server.py
@@ -22,5 +22,5 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "/var/www/Fiptster-Web-APP/media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "/var/www/media/")
 
