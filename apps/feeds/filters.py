b50f955cd2cdf6fc96504165959eec70a2eb08f8
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2025-05-30 16:34:21 +0500

    chnaged

--- apps/feeds/filters.py
+++ apps/feeds/filters.py
@@ -4,7 +4,7 @@ from apps.feeds.models import Feed
 
 
 class FeedFilter(django_filters.FilterSet):
-    category = django_filters.UUIDFilter(field_name='category')
+    category = django_filters.UUIDFilter(field_name='category__uuid')
 
     class Meta:
         model = Feed
